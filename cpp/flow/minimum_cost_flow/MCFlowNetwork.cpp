47d723c30818421d0dc47a416c9f219f97bcd5bf
Oskar Krygier <oskar.krygier@enterit.pl>
2026-03-15 20:32:51 +0100

    Add min-cost flow network structure

---
+++ cpp/flow/minimum_cost_flow/MCFlowNetwork.cpp
@@ -0,0 +1,137 @@
+#include <minimum_cost_flow/MCFlowNetwork.hpp>
+#include <limits>
+
+namespace Koala {
+    using node = NetworKit::node;
+    using Edge = NetworKit::Edge;
+    using Graph = NetworKit:: Graph;
+    using int64 = std::int64_t;
+
+    MCFlowNetwork::MCFlowNetwork(Graph const& g) : graph(g) {
+        if (graph.isWeighted()) {
+            graph.forEdges([&](node u, node v, NetworKit::edgeweight weight, NetworKit::edgeid _) {
+                capacity[{u, v}] = static_cast<int64>(weight < 0 ? weight - 0.5 : weight + 0.5);
+            });
+        } else {
+            graph.forEdges([&](node u, node v) {
+                capacity[{u, v}] = std::numeric_limits<int64>::max();
+            });
+        }
+    }
+
+    MCFlowNetwork::MCFlowNetwork(Graph const& g, std::unordered_map<node, int64> const& ex) 
+        : MCFlowNetwork(g) {
+        excess = ex;
+        primaryExcess = ex;
+    }
+    MCFlowNetwork::MCFlowNetwork(Graph const& g, std::unordered_map<node, int64> const& ex,
+        std::unordered_map<Edge, int> cost) : MCFlowNetwork(g, ex) {
+        cost = cost;
+    }
+
+    Graph const& MCFlowNetwork::getGraph() const {
+        return graph;
+    };
+
+    node MCFlowNetwork::addNode(int ex = 0) {
+        node newNode = graph.addNode();
+        excess[newNode] = ex;
+        return newNode;
+    }
+
+    void MCFlowNetwork::addEdge(node s, node t, int cost = 0, int64 capacity = 0) {
+        graph.addEdge(s, t, capacity);
+        if (graph.isWeighted())
+            this->capacity[{s,t}] = capacity;
+        this->cost[{s,t}] = cost; 
+    }
+
+    void MCFlowNetwork::pushFlow(node s, node t, int64 flow) {
+        int64 maxAdd = capacity[{s,t}] - this->flow[{s,t}];
+        flow = !uncapacitated ? std::min(maxAdd, flow) : flow;
+        excess[s] -= flow;
+        excess[t] += flow;
+        this->flow[{s,t}] += flow;
+        this->flow[{t,s}] -= flow;
+    }
+
+    void MCFlowNetwork::setFlow(node s, node t, int64 value) {
+        int64 diff = value - flow[{s,t}];
+        flow[{s,t}] += diff;
+        flow[{t,s}] -= diff;
+        excess[s] -= diff;
+        excess[t] += diff;
+    }
+
+    int64 MCFlowNetwork::getFlow(node s, node t) const {
+        auto it = flow.find({s,t});
+        if (it != flow.end())
+            return it->second;
+        return 0;
+    }
+
+    int MCFlowNetwork::getCost(node s, node t) const {
+        auto it = cost.find({s,t});
+        if (it != cost.end())
+            return it->second;
+        return 0;
+    }  
+
+    void MCFlowNetwork::setCost(node s, node t, int cost) {
+        this->cost[{s,t}] = cost;
+    }
+
+    int64 MCFlowNetwork::getCapacity(node s, node t) const {
+        auto it = capacity.find({s,t});
+        if (it != capacity.end())
+            return it->second;
+        return 0;
+    }
+
+    void MCFlowNetwork::setCapacity(node s, node t, int64 value) {
+        capacity[{s,t}] = value;
+    }
+
+    int64 MCFlowNetwork::getExcess(node u) const {
+        auto it = excess.find(u);
+        if (it != excess.end())
+            return it->second;
+        return 0;
+    }
+
+    void MCFlowNetwork::setExcess(node u, int64 ex) {
+        int64 diff = ex - primaryExcess[u];
+        primaryExcess[u] = ex;
+        excess[u] += diff;
+    }
+
+    void MCFlowNetwork::forResidualEdgesOf(node u, std::function<void(NetworKit::node, NetworKit::node)> const& func) const {
+        graph.forNeighborsOf(u, [&](node v) {
+            int64 residualCapacity = 1;
+            
+            if (!uncapacitated) {
+                auto capacityIt = capacity.find({u,v});
+                int64 vCapacity = capacityIt != capacity.end() ? capacityIt->second : 0;
+                auto flowIt = flow.find({u,v});
+                int64 vFlow = flowIt != flow.end() ? flowIt->second : 0;
+                residualCapacity = vCapacity - vFlow;
+            }
+
+            if (residualCapacity > 0) {
+                func(u, v);
+            }
+        });
+
+        graph.forInNeighborsOf(u, [&](node v) {
+            auto flowIt = flow.find({u,v});
+            int64 vFlow = flowIt != flow.end() ? flowIt->second : 0;
+
+            int64 residualCapacity = vFlow;
+            if (residualCapacity > 0) {
+                func(u, v);
+            }
+        });
+    }
+
+} /* namespace Koala */
+
---
+++ include/flow/minimum_cost_flow/MCFlowNetwork.hpp
@@ -0,0 +1,66 @@
+#include <networkit/graph/Graph.hpp>
+#include <networkit/graph/EdgeIterators.hpp>
+#include <unordered_map>
+#include <vector>
+namespace Koala {
+
+class BaseMCFlowNetwork {
+ public:
+    virtual NetworKit::Graph const& getGraph() const = 0;
+    virtual NetworKit::node addNode(int ex = 0) = 0;
+    virtual void addEdge(NetworKit::node s, NetworKit::node t, int cost = 0, std::int64_t capacity = 0) = 0;
+    
+    virtual void pushFlow(NetworKit::node s, NetworKit::node t, std::int64_t value) = 0;
+    virtual void setFlow(NetworKit::node s, NetworKit::node t, std::int64_t value) = 0;
+    virtual std::int64_t getFlow(NetworKit::node s, NetworKit::node t) const = 0;
+    
+    virtual int getCost(NetworKit::node s, NetworKit::node t) const = 0;
+    virtual void setCost(NetworKit::node s, NetworKit::node t, int cost) = 0;
+
+    virtual std::int64_t getCapacity(NetworKit::node s, NetworKit::node t) const = 0;
+    virtual void setCapacity(NetworKit::node s, NetworKit::node t, std::int64_t value) = 0;
+    
+    virtual std::int64_t getExcess(NetworKit::node u) const = 0;
+    virtual void setExcess(NetworKit::node u, std::int64_t ex) = 0;
+
+    virtual void forResidualEdgesOf(NetworKit::node u, std::function<void(NetworKit::node, NetworKit::node)> const& func) const;
+};
+
+class MCFlowNetwork : public BaseMCFlowNetwork {
+    std::unordered_map<NetworKit::Edge, std::int64_t> flow;
+    std::unordered_map<NetworKit::Edge, int> cost;
+    std::unordered_map<NetworKit::Edge, int> capacity;
+
+    std::unordered_map<NetworKit::node, std::int64_t> primaryExcess;
+    std::unordered_map<NetworKit::node, std::int64_t> excess;
+    NetworKit::Graph graph;
+    bool uncapacitated = false;
+ public:
+    
+    MCFlowNetwork(NetworKit::Graph const& g);
+    MCFlowNetwork(NetworKit::Graph const& g, std::unordered_map<NetworKit::node, std::int64_t> const& ex);
+    MCFlowNetwork(NetworKit::Graph const& g, std::unordered_map<NetworKit::node, std::int64_t> const& ex,
+        std::unordered_map<NetworKit::Edge, int> cost);
+
+    virtual NetworKit::Graph const& getGraph() const override;
+
+    virtual NetworKit::node addNode(int ex = 0) override;
+    virtual void addEdge(NetworKit::node s, NetworKit::node t, int cost = 0, std::int64_t capacity = 0) override;
+    
+    virtual void pushFlow(NetworKit::node s, NetworKit::node t, std::int64_t value) override;
+    virtual void setFlow(NetworKit::node s, NetworKit::node t, std::int64_t value) override;
+    virtual std::int64_t getFlow(NetworKit::node s, NetworKit::node t) const override;
+    
+    virtual int getCost(NetworKit::node s, NetworKit::node t) const override;
+    virtual void setCost(NetworKit::node s, NetworKit::node t, int cost) override;
+
+    virtual std::int64_t getCapacity(NetworKit::node s, NetworKit::node t) const override;
+    virtual void setCapacity(NetworKit::node s, NetworKit::node t, std::int64_t value) override;
+    
+    virtual std::int64_t getExcess(NetworKit::node u) const override;
+    virtual void setExcess(NetworKit::node u, std::int64_t ex) override;
+
+    virtual void forResidualEdgesOf(NetworKit::node u, std::function<void(NetworKit::node, NetworKit::node)> const& func) const override;
+};
+
+}
