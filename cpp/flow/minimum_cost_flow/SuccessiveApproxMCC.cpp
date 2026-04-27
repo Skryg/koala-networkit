8d0aaaf034dc594fce5e1d476d4ddb8639b8abb3
Oskar Krygier <oskar.krygier@enterit.pl>
2026-04-28 00:18:18 +0200

    Change successive approximation algorithm

--- cpp/flow/minimum_cost_flow/SuccessiveApproxMCC.cpp
+++ cpp/flow/minimum_cost_flow/SuccessiveApproxMCC.cpp
@@ -2,94 +2,70 @@
 #include <climits>
 namespace Koala {
 
-using edgeid = NetworKit::edgeid;
 using node = NetworKit::node;
-using Graph = NetworKit::Graph;
 
-// static void print_flows(edge_map<int> &printable);
-static void forEachArcOf(Graph const&, node u, std::function<void(node, node, edgeid, bool)> f);
-
-void SuccessiveApproxMCC::initFlow() {
-    constructCirculationFromFlow();
-    initCirculation();
+inline double SuccessiveApproxMCC::cp(uint64_t eid) {
+    const Edge& edge = edges[eid];
+    return static_cast<double>(edge.cost) - potential[edge.from] + potential[edge.to];
 }
 
-void SuccessiveApproxMCC::initCirculation() {}
-
-inline double SuccessiveApproxMCC::cp(node v,
-    node w, edgeid eid) {
-    return static_cast<double>(costs[eid]) - potential[v] + potential[w];
+inline int64_t SuccessiveApproxMCC::uf(uint64_t eid) {
+    return edges[eid].capacity - edges[eid].cost;
 }
 
-inline int SuccessiveApproxMCC::uf(edgeid eid, bool reversed = false) {
-    if(!reversed)
-        return upperbound[eid] - flow[eid];
-    return flow[eid] - lowerbound[eid];
+bool SuccessiveApproxMCC::is_imbalanced() {
+    for (auto e : excess) {
+        if (e) return true;
+    }
+    return false;
 }
 
-void SuccessiveApproxMCC::force_flow(node v, node w,
-    edgeid eid, int f) {
-    flow[eid] += f;
-    excess[v] -= f;
-    excess[w] += f;
+void SuccessiveApproxMCC::force_flow(uint64_t eid, int64_t f) {
+    Edge& edge = edges[eid];
+    edge.flow += f;
+    edges[eid ^ 1].flow -= f;
+    excess[edge.from] -= f;
+    excess[edge.to] += f;
 }
 
-void SuccessiveApproxMCC::push(node v, node w, edgeid eid, bool reverse = false) {
-    if (!reverse) { 
-        int residualCapacity = upperbound[eid] - flow[eid];
-        int change = std::min(residualCapacity, excess[v]);
-        force_flow(v, w, eid, change);
-    } else {
-        int residualCapacity = flow[eid] - lowerbound[eid];
-        int change = std::min(residualCapacity, excess[w]);
-        force_flow(v, w, eid, -change);
+void SuccessiveApproxMCC::push(uint64_t eid) {
+    node u = edges[eid].from;
+    if (excess[u] > 0) {
+        force_flow(eid, std::min(uf(eid), excess[u]));
     }
 }
 
-void SuccessiveApproxMCC::relabel(NetworKit::node const& v) {
+void SuccessiveApproxMCC::relabel(NetworKit::node const& u) {
     double mi = std::numeric_limits<double>::max();
-    graph.forEdgesOf(
-        v,
-        [&](node i, node j, edgeid eid) {
-            if (uf(eid) > 0) {
-                mi = std::min(mi, potential[j]
-                    + epsi
-                    + costs[eid]);
-            }
-        });
-    graph.forInEdgesOf(
-        v,
-        [&](node i, node j, edgeid eid) {
-            if (uf(eid, true) > 0) {
-                mi = std::min(mi, potential[j]
-                    + epsi
-                    - costs[eid]);
-            }
-        });
-    potential[v] = mi;
+
+    for (uint64_t eid : neigh_list[u]) {
+        if (uf(eid) > 0) {
+            const Edge& edge = edges[eid];
+            mi = std::min(mi, potential[edge.to] + epsi + edge.cost);
+        }
+    }
+
+    potential[u] = mi;
 }
 
 void SuccessiveApproxMCC::refine() {
     epsi /= 2;
-    graph.forEdges([&](node v, node w, edgeid eid) {
-        double reducedCost = cp(v, w, eid);
-        if (reducedCost < 0) {
-            int residualCap = upperbound[eid] - flow[eid];
-            force_flow(v, w, eid, residualCap);
-        } else if (reducedCost > 0) {
-            int residualCap = flow[eid] - lowerbound[eid];
-            force_flow(v, w, eid, -residualCap); 
+    for (uint64_t eid = 0; eid < edges.size(); ++eid) {
+        double reduced = cp(eid);
+        if (reduced < 0) {
+            force_flow(eid, uf(eid));
         }
-    });
-
+    }
     wave();
 }
+
+
 
 void SuccessiveApproxMCC::wave() {
     DischargeList* list = new ToposortList(*this);
 
     NetworKit::node v = list->getNext();
-    while (active.size()) {
+    while (is_imbalanced()) {
         if (excess[v] > 0) {
             bool relabeled = discharge(v);
             if (relabeled) {
@@ -102,20 +78,13 @@ void SuccessiveApproxMCC::wave() {
 }
 
 bool SuccessiveApproxMCC::discharge(NetworKit::node const& u) {
-    int& ex = excess[u];
-    graph.forEdgesOf(u, [&](node u, node v, edgeid eid) {
-        if (ex && cp(u, v, eid) < 0 && uf(eid) > 0) {
-            push(u, v, eid);
+    int64_t& ex = excess[u];
+    for (uint64_t eid : neigh_list[u]) {
+        if (ex && cp(eid) < 0 && uf(eid) > 0) {
+            push(eid);
         }
-    });
-    if (ex > 0) {
-        // reversed arcs, that enters u can be outgoing in residual graph
-        graph.forInEdgesOf(u, [&](node u, node v, node eid) {
-            if (ex && cp(u, v, eid) > 0 && uf(eid, true) > 0) {
-                push(u, v, eid, true);
-            }
-        });
     }
+
     if (ex > 0) {
         relabel(u);
         return true;
@@ -125,77 +94,107 @@ bool SuccessiveApproxMCC::discharge(NetworKit::node const& u) {
 }
 
 void SuccessiveApproxMCC::initialize() {
+    auto& graph = network.getGraph();
+    uint32_t nodeBound = graph.upperNodeIdBound();
+    nodes_number = graph.numberOfNodes();
     potential.clear();
-    excess.clear();
-    flow.clear();
-    active.clear();
-    pr_id.clear();
-    feasible = true;
-    // std::cerr<<"INIT"<<"\n";
-    for (auto [v,e] : excess) {
-        if (e > 0) {
-            active.insert(v);
-        }
+    excess.assign(nodeBound, 0);
+   
+    for (auto [key, value] : network.excess) {
+        excess[key] = value;
     }
 
-    int mx = 0;
+    potential.assign(nodeBound, 0);
+    edges.reserve(2 * graph.numberOfEdges());
+    int ptr = 0;
+    
+    neigh_list.assign(nodeBound, std::vector<uint64_t>());
+    std::set<std::pair<NetworKit::node, NetworKit::node>> present;
+    int64_t maxCost = 0;
 
-    for (auto [_, cost] : costs) {
-        mx = std::max(mx, abs(cost));
-    }
-    epsi = static_cast<double>(mx);
 
-    graph.forEdges([&](node u, node v, edgeid eid) {
-        int cap;
-        if ((cap = upperbound[eid]) < 0) {
-            force_flow(u, v, eid, cap);
-        } else if ((cap = lowerbound[eid] > 0)) {
-            force_flow(u, v, eid, cap);
-        }
+    graph.forNodes([&](node u) { 
+        graph.forNeighborsOf(u, [&](node v) {
+            node x = std::min(u, v);
+            node y = std::max(u, v);
+            if (present.find({x, y}) != present.end())
+                return;
+            present.insert({x, y});
+
+            uint32_t from = static_cast<uint32_t>(u);
+            uint32_t to = static_cast<uint32_t>(v);
+            int64_t cost = network.cost[{u, v}] - network.cost[{v, u}];
+            int64_t capacity = network.capacity[{u, v}];
+            int64_t capacity2 = network.capacity[{v, u}];
+
+            neigh_list[from].push_back(edges.size());
+
+            int64_t fl = std::min(capacity, (int64_t)0);
+            fl = capacity2 < 0 ? -capacity2 : fl;
+
+            edges.push_back({
+                from, to,
+                cost, capacity, fl
+            });
+
+            maxCost = std::max(maxCost, std::abs(cost));
+
+            neigh_list[to].push_back(edges.size());
+            edges.push_back({
+                to, from,
+                -cost, capacity2, -fl
+            });
+
+        });
     });
+
+    epsi = static_cast<double>(maxCost);
 }
 
-void SuccessiveApproxMCC::runImpl() {
+void SuccessiveApproxMCC::run_impl() {
     initialize();
 
-    while (epsi >= 1.0/graph.numberOfNodes()) {
+    while (epsi >= 1.0/nodes_number) {
         refine();
-        // print_flows(flow);
     }
 
     min_cost = 0;
     
-    graph.forEdges([&](node u, node v, edgeid eid) {
-        min_cost += flow[eid] * costs[eid];
-    });
+    for (const Edge& edge : edges) {
+        min_cost += edge.flow * edge.cost;
+        computed_flow[{edge.from, edge.to}] = edge.flow;
+    }
+    min_cost /= 2;
 }
 
 
 SuccessiveApproxMCC::ToposortList::ToposortList(
     SuccessiveApproxMCC &approx) : approx(approx) {
-    vis.clear();
-    for (auto v : approx.graph.nodeRange()) {
+    vis.assign(approx.nodes_number, 0);
+    auto& graph = approx.network.getGraph();
+    for (auto v : graph.nodeRange()) {
         if(!vis[v]) dfs(v);
     }
     it2 = nodes.begin();
 }
 
-void SuccessiveApproxMCC::ToposortList::dfs(NetworKit::node v){
-    vis[v] = true;
+void SuccessiveApproxMCC::ToposortList::dfs(NetworKit::node u){
+    vis[u] = true;
 
-    forEachArcOf(approx.graph, v, [&](node i, node j, edgeid id, bool reversed) {
-        if (approx.cp(i, j, id)*(reversed ? -1 : 1)  < 0 && approx.uf(id, reversed)) {
-            if (!vis[j]) {
-                dfs(j);
+    for (auto eid : approx.neigh_list[u]) {
+        if (approx.cp(eid) < 0 && approx.uf(eid) > 0) {
+            auto v = approx.edges[eid].to;
+            if (!vis[v]) {
+                dfs(v);
             }
+            
         }
-    });
-    
-    nodes.push_front(v);
+    }
+
+    nodes.push_front(u);
 }
 
 NetworKit::node SuccessiveApproxMCC::ToposortList::getNext() {
-    // std::cerr<<"NEXT\n";
     if (it2 != nodes.end()) {
         it1 = it2;
         it2++;
@@ -208,11 +207,12 @@ NetworKit::node SuccessiveApproxMCC::ToposortList::getNext() {
 
 void SuccessiveApproxMCC::ToposortList::moveToStart() {
     if (it1 != nodes.end()) {
-        
-        NetworKit::node value = *it1;
-        nodes.erase(it1);
-        nodes.push_front(value);
+        nodes.splice(nodes.begin(), nodes, it1);
     }
+}
+
+int64_t SuccessiveApproxMCC::getFlow(const NetworKit::Edge& edge) {
+    return computed_flow[edge];
 }
 
 // static void print_flows(edgeid_map<int> &printable) {
@@ -222,20 +222,5 @@ void SuccessiveApproxMCC::ToposortList::moveToStart() {
 //     }
 //     // std:: cerr<< " --- PRINT END --- \n\n";
 // }
-
-static void forEachArcOf(
-    Graph const& g,
-    node u,
-    std::function<void(node, node, edgeid, bool)> f) {
-    
-    g.forEdgesOf(u, [&](node u, node v, edgeid id) {
-        f(u, v, id, false);
-    });
-
-    g.forInEdgesOf(u, [&](node u, node v, edgeid id) {
-        f(u, v, id, true);
-    });
-}
-
 
 } /* namespace Koala */
--- include/flow/minimum_cost_flow/SuccessiveApproxMCC.hpp
+++ include/flow/minimum_cost_flow/SuccessiveApproxMCC.hpp
@@ -3,74 +3,65 @@
 #include <set>
 #include <unordered_set>
 #include <list>
+#include <cstdint>
 
 #include <flow/MinimumCostFlow.hpp>
+#include <flow/minimum_cost_flow/MCFlowNetwork.hpp>
 
 namespace Koala {
 
 class SuccessiveApproxMCC final : public MinimumCostFlow {
- public:   
-   SuccessiveApproxMCC(NetworKit::Graph const& g, edgeid_map<long long> const& costs,
-      NetworKit::node s, NetworKit::node t, long long f) : MinimumCostFlow(g, costs, s, t, f) {
-        initFlow();
-      }
-   // flow
-   SuccessiveApproxMCC(NetworKit::Graph const& g, edgeid_map<long long> const& costs,
-         node_map<long long> const& b) : MinimumCostFlow(g, costs, b) {
-         initFlow();
-      }
-   // circulation
-   SuccessiveApproxMCC(NetworKit::Graph const& g, edgeid_map<std::pair<long long,long long>> const& bounds,
-         edgeid_map<long long> const& costs) : MinimumCostFlow(g, bounds, costs) {
-         initCirculation();
-      }
+   struct Edge {
+      uint32_t from, to;
+      int64_t cost, capacity, flow;
+   };
+   std::vector<Edge> edges;
+   std::vector<std::vector<uint64_t>> neigh_list;
+   
+   void run_impl() override;
+   bool is_imbalanced();
+   void initialize();
+   void push(uint64_t eid);
+   void relabel(NetworKit::node const&);
+   void refine();
+   void wave();
+   bool discharge(NetworKit::node const&);
+   
+   double cp(uint64_t eid);
+   int64_t uf(uint64_t eid);
+   void force_flow(uint64_t eid, int64_t f);
+   std::vector<double> potential;
+   std::vector<int64_t> excess;
+   uint32_t nodes_number{0};
+   
+   std::unordered_map<NetworKit::Edge, int64_t> computed_flow; 
 
-
- private:
-    void runImpl() override;
-    void initFlow() override;
-    void initCirculation() override;
-
-    void initialize();
-    void push(NetworKit::node, NetworKit::node, NetworKit::edgeid, bool reversed);
-    void relabel(NetworKit::node const&);
-    void refine();
-    void wave();
-    bool discharge(NetworKit::node const&);
-
-    double cp(NetworKit::node, NetworKit::node, NetworKit::edgeid);
-    int uf(NetworKit::edgeid, bool reversed);
-    void force_flow(NetworKit::node, NetworKit::node, NetworKit::edgeid, int);
-    node_map<double> potential;
-    node_map<int> excess;
-    node_map<int> pr_id;
-    edgeid_map<int> lowerbound, upperbound;
-    std::unordered_set<NetworKit::node> active;
-
-    double epsi{0.};
-
-    class DischargeList {
-     public:
-        virtual NetworKit::node getNext() { return 0; }
-        virtual void moveToStart() {}
-    };
-
-    class ToposortList : public DischargeList {
-     public:
-        ToposortList(SuccessiveApproxMCC&);
-        
-        NetworKit::node getNext() override;
-        void moveToStart() override;
-     private:
-        SuccessiveApproxMCC &approx;
-        std::list<NetworKit::node> nodes;
-        node_map<bool> vis; 
-        void dfs(NetworKit::node);
-
-        std::list<NetworKit::node>::iterator it1;
-        std::list<NetworKit::node>::iterator it2;
-    };
-
+   double epsi{0.};
+ 
+   class DischargeList {
+      public:
+      virtual NetworKit::node getNext() { return 0; }
+      virtual void moveToStart() {}
+   };
+      
+   class ToposortList : public DischargeList {
+    public:
+      ToposortList(SuccessiveApproxMCC&);
+      
+      NetworKit::node getNext() override;
+      void moveToStart() override;
+    private:
+      SuccessiveApproxMCC &approx;
+      std::list<NetworKit::node> nodes;
+      std::vector<bool> vis; 
+      void dfs(NetworKit::node);
+      
+      std::list<NetworKit::node>::iterator it1;
+      std::list<NetworKit::node>::iterator it2;
+   };
+ public:
+   SuccessiveApproxMCC(const MCFlowNetwork& network) : MinimumCostFlow(network) {}
+   int64_t getFlow(const NetworKit::Edge& edge);
 };
 
 } /* namespace Koala */
